5774117cd701a4bfd317c6c7f4c48f99a1f9df9d
Dilanga D Amarasinghe <73058707+DoubleDAmarasinghe@users.noreply.github.com>
2020-11-09 21:40:05 +0530

    Add files via upload

---
+++ Sample_List_Application/Task3.cs
@@ -0,0 +1,179 @@
+/*´´´´´´´´´´´´´´´´´´´ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´`
+´´´´´´´´´´´´´´´´´¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´´¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´
+´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´`´´´´´´´´´´´¶¶´´´´´´´´´´`
+´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´
+´´´´´´´´´´´¶¶´¶¶´´´¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´¶¶´¶¶´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶¶¶´´´´´´´´´´´
+...........................................................
+Author   - DoubleD Amarasinghe			          .				   
+E-mail   - doubledamarasinghe@gmail.com			  .
+GitHub   - https://github.com/DoubleDAmarasinghe	  .
+whatsApp - +960 969 5658 	   .                      .
+...........................................................
+´´´´´´´´´´´´´¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶´´´´´´´´´´´´´
+´´´´¶¶¶´´´´´´´¶¶´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´¶¶´´´´´´¶¶¶¶´´´
+´´´¶¶¶¶¶´´´´´¶¶´´´¶¶¶¶¶¶¶´´´¶¶¶´´´¶¶¶¶¶¶¶´´´¶¶´´´´´¶¶¶¶¶¶´´
+´´¶¶´´´¶¶´´´´¶¶´´´´´¶¶¶´´´´¶¶¶¶¶´´´´¶¶¶´´´´´¶¶´´´´¶¶´´´¶¶´´
+´¶¶¶´´´´¶¶¶¶´´¶¶´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´¶¶¶¶´´´´¶¶¶´
+¶¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶
+¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶¶´´´´´´¶¶¶¶¶¶¶¶
+´´¶¶¶¶´¶¶¶¶¶´´´´´´¶¶¶¶¶´´´´´´´´´´´´´´¶¶¶´¶¶´´´´´¶¶¶¶¶¶´¶¶¶´
+´´´´´´´´´´¶¶¶¶¶¶´´¶¶¶´´¶¶´´´´´´´´´´´¶¶´´¶¶¶´´¶¶¶¶¶¶´´´´´´´´
+´´´´´´´´´´´´´´¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´´´¶¶´¶¶´¶´¶´¶´¶´¶´¶´¶´¶´¶¶´´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´¶¶¶¶´´¶´¶´¶´¶´¶´¶´¶´¶´´´¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶¶´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶´¶¶¶¶¶´´´´´´´´´´´´
+´´´´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´´´´´´´´´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶´´´´
+´´´¶¶´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´´
+´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶´´´´
+´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´
+´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´
+´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´*/
+using System; 
+using System.Drawing; 
+using System.Windows.Forms;
+class MyForm : System.Windows.Forms.Form
+{
+	Form frm = new Form();
+	Label l1 = new Label();
+	Label l2 = new Label();
+	TextBox txt = new TextBox();
+	TextBox txt2 = new TextBox();
+	Button btn1 = new Button();
+	Button btn6 = new Button();
+	Button btn2 = new Button();
+	Button btn4 = new Button();
+	Button btn3 = new Button();
+	Button btn5 = new Button();
+	MyForm()
+	{
+		frm.Text = "Sample List Application"; 
+		frm.Size = new Size(700,300); 
+		frm.StartPosition = FormStartPosition.CenterScreen; 
+		frm.Opacity = 100.0; 
+		frm.AutoSize = true; 
+		frm.FormBorderStyle = FormBorderStyle.FixedDialog; 
+		
+		l1.Text = "Enter List of Values Read from a Text file";
+		l1.Size = new Size(300, 25);
+		l1.Location = new Point(10, 15);
+		
+		l2.Text = "Result/ Answer";
+		l2.Size = new Size(300, 25);
+		l2.Location = new Point(10, 190);
+		
+		txt.Location = new Point(10,30); 
+		txt.Size = new Size(350,150); 
+		txt.Multiline = true; 
+		txt.AutoSize = true; 
+		
+		txt2.Location = new Point(10,210); 
+		txt2.Size = new Size(350,10); 
+		txt2.Multiline = false; 
+		txt2.AutoSize = true; 
+		 
+		btn1.Text = "Read data from file"; 
+		btn1.Location = new Point(400,15); 
+		btn1.Size = new Size(260,15); 
+		btn1.AutoSize = true; 
+		btn1.MouseClick += read; 
+	
+		btn2.Text = "Sum"; 
+		btn2.Location = new Point(400,75); 
+		btn2.Size = new Size(100,15); 
+		btn2.AutoSize = true; 
+		btn2.MouseClick += sum; 
+		
+		btn3.Text = "Product"; 
+		btn3.Location = new Point(560,75); 
+		btn3.Size = new Size(100,15); 
+		btn3.AutoSize = true; 
+		btn3.MouseClick += product; 
+		
+		btn4.Text = "Mean"; 
+		btn4.Location = new Point(400,120); 
+		btn4.Size = new Size(100,15); 
+		btn4.AutoSize = true; 
+		btn4.MouseClick += mean; 
+		
+		btn5.Text = "Standard Daviation"; 
+		btn5.Location = new Point(560,120); 
+		btn5.Size = new Size(100,15); 
+		btn5.AutoSize = true; 
+		
+		btn6.Text = "Reset"; 
+		btn6.Location = new Point(400,185); 
+		btn6.Size = new Size(260,15); 
+		btn6.AutoSize = true; 
+		btn6.MouseClick += reset; 
+		
+		frm.Controls.Add(btn6);
+		frm.Controls.Add(btn5);
+		frm.Controls.Add(btn4);
+		frm.Controls.Add(btn3);
+		frm.Controls.Add(btn2);
+		frm.Controls.Add(btn1);
+		frm.Controls.Add(txt2);
+		frm.Controls.Add(l2);
+		frm.Controls.Add(txt);
+		frm.Controls.Add(l1);
+		frm.ShowDialog();
+	}
+	
+	void reset(Object sender,EventArgs e) 
+	{ 
+		txt.Text = "";
+		txt2.Text = "";
+	}
+	
+	void read(Object sender,EventArgs e) 
+	{ 
+		string[] sep = new string[] {"\r\n"};
+		string[] lines = txt.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+		txt2.Text = lines[0] +","+ lines[1];
+	}
+	
+	void sum(Object sender,EventArgs e) 
+	{ 
+		string[] sep = new string[] {"\r\n"};
+		string[] lines = txt.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+		int num1 = Convert.ToInt32(lines[0]);
+		int num2 = Convert.ToInt32(lines[1]);
+		int sum = num1 + num2;
+		txt2.Text = Convert.ToString(sum);
+	}
+	
+	void product(Object sender,EventArgs e) 
+	{ 
+		string[] sep = new string[] {"\r\n"};
+		string[] lines = txt.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+		int num1 = Convert.ToInt32(lines[0]);
+		int num2 = Convert.ToInt32(lines[1]);
+		int sum = num1 * num2;
+		txt2.Text = Convert.ToString(sum);
+	}
+	
+	void mean(Object sender,EventArgs e) 
+	{ 
+		string[] sep = new string[] {"\r\n"};
+		string[] lines = txt.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+		int num1 = Convert.ToInt32(lines[0]);
+		int num2 = Convert.ToInt32(lines[1]);
+		int sum = (num1 + num2)/2;
+		txt2.Text = Convert.ToString(sum);
+	}
+	
+	public static void Main()
+	{
+		MyForm obj = new MyForm();
+		
+	}
+}
